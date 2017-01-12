056eebe07a4b75f287d275c4dff42a3a9224ed95
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2017-01-12 14:13:32 +0100

    Add test on FieldPlaceholder

---
+++ src/test/java/ndr/brt/sieve/FieldPlaceholderTest.java
@@ -0,0 +1,27 @@
+package ndr.brt.sieve;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FieldPlaceholderTest {
+
+    @Test
+    public void replace_placeholders() throws Exception {
+        String string = "This object has id {{id}} and it's called {{name}}";
+
+        String result = FieldPlaceholder.substitutePlaceholders(string, new Pojo("any name", 42L));
+
+        assertThat(result).isEqualTo("This object has id 42 and it's called any name");
+    }
+
+    private class Pojo {
+        private final String name;
+        private final Long id;
+
+        private Pojo(String name, Long id) {
+            this.name = name;
+            this.id = id;
+        }
+    }
+}
