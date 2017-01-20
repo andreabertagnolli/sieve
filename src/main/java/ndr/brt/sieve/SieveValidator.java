db0e8d9b957cd26f49406baa17c768fb670d4934
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2017-01-20 13:39:37 +0100

    Move NestedReference as private class of sieve validator

--- src/main/java/ndr/brt/sieve/SieveValidator.java
+++ src/main/java/ndr/brt/sieve/SieveValidator.java
@@ -62,4 +62,27 @@ public class SieveValidator<T> {
     public List<PredicateValidator<T>> getValidators() {
         return validators;
     }
+
+    private static class NestedReference<T, N> {
+
+        private final Function<T, List<N>> getNested;
+        private List<PredicateValidator<N>> validators = new ArrayList<>();
+
+        public NestedReference(Function<T, List<N>> getNested) {
+            this.getNested = getNested;
+        }
+
+        public NestedReference(Function<T, List<N>> nested, SieveValidator<N> validator) {
+            this.getNested = nested;
+            this.validators.addAll(validator.getValidators());
+        }
+
+        public List<N> getObjects(T object) {
+            return getNested.apply(object);
+        }
+
+        public List<PredicateValidator<N>> getValidators() {
+            return validators;
+        }
+    }
 }
