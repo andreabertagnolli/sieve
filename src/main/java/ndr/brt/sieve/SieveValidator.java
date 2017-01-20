8c9b525b51428f5bfda3683da398e78d1d0041da
Andrea Bertagnolli <andrea.bertagnolli@paros.srl>
2017-01-20 13:52:32 +0100

    Use identity function

--- src/main/java/ndr/brt/sieve/SieveValidator.java
+++ src/main/java/ndr/brt/sieve/SieveValidator.java
@@ -7,6 +7,7 @@ import java.util.function.Function;
 import java.util.stream.Stream;
 
 import static java.util.Collections.singletonList;
+import static java.util.function.Function.identity;
 import static java.util.stream.Stream.concat;
 
 public class SieveValidator<T> {
@@ -26,27 +27,27 @@ public class SieveValidator<T> {
         return concat(
                 objects.stream()
                         .map(this::validateRoot)
-                        .flatMap(e -> e),
+                        .flatMap(identity()),
                 objects.stream()
                         .map(this::validateNested)
-                        .flatMap(e -> e)
+                        .flatMap(identity())
         );
     }
 
     private Stream<Bran> validateRoot(T object) {
         return validators.stream()
                 .map(v -> v.validate(object))
-                .flatMap(e -> e);
+                .flatMap(identity());
     }
 
     private Stream<Bran> validateNested(T object) {
         return nestedReferences.stream()
                 .map(n -> n.getValidators().stream()
                     .map(v -> v.validate((List) n.getObjects(object)))
-                    .flatMap(e -> e)
+                    .flatMap(identity())
                     .map(Bran.class::cast)
                 )
-                .flatMap(e -> e);
+                .flatMap(identity());
     }
 
     public SieveValidator<T> with(PredicateValidator<T> validator) {
